2a39760db228c6eb6da215f4a2a556ade40c3a8e
Mohamad Alaa Baghdadi <newmhd.alaa.bg@gmail.com>
2022-02-21 02:20:11 +0300

    Home(Products Screen)

---
+++ lib/features/home/presentation/widgets/carousel_slider.dart
@@ -0,0 +1,30 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:salla/features/home/data/models/home_model.dart';
+
+class CarouselSliderBuilder extends StatelessWidget {
+  final HomeModel? homeDate;
+  const CarouselSliderBuilder({Key? key,required this.homeDate}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      items: homeDate!.data.banners
+          .map((e) => Image.network(e.image))
+          .toList(),
+      options: CarouselOptions(
+        autoPlay: true,
+        viewportFraction: 1,
+        height: 250,
+        autoPlayInterval: const Duration(seconds: 2),
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enableInfiniteScroll: true,
+        initialPage: 0,
+        pauseAutoPlayOnTouch: true,
+
+
+      ),
+    );
+  }
+}
