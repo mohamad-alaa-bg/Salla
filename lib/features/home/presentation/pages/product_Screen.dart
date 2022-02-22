efd9f143c5e503846d773e3dc6c5d678897a7540
Mohamad Alaa Baghdadi <newmhd.alaa.bg@gmail.com>
2022-02-22 23:34:17 +0300

    Home(Products Screen Grid)

--- lib/features/home/presentation/pages/product_Screen.dart
+++ lib/features/home/presentation/pages/product_Screen.dart
@@ -1,9 +1,8 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:salla/features/home/presentation/bloc/home_bloc.dart';
 import 'package:salla/features/home/presentation/widgets/carousel_slider.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:salla/features/home/presentation/widgets/product_grid_view.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key? key}) : super(key: key);
@@ -11,24 +10,28 @@ class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var bloc = HomeBloc.get(context);
-    int indexC =0;
+    int indexC = 0;
     return BlocBuilder<HomeBloc, HomeState>(
       buildWhen: (previous, current) => current is HomePageDataSuccess,
       builder: (context, state) {
         return bloc.homeData == null
             ? const Center(
                 child: CircularProgressIndicator(),
-
               )
-            : SizedBox(
-                width: double.infinity,
-                child: Column(
+            : SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
                   children: [
                     CarouselSliderBuilder(homeDate: bloc.homeData),
-
+                    SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: productGridView(homeData: bloc.homeData),
+                      ),
+                    ),
                   ],
                 ),
-              );
+            );
       },
     );
   }
