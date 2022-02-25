b99c006ff0330f64a03e17a82e868322aaf47bee
Mohamad Alaa Baghdadi <newmhd.alaa.bg@gmail.com>
2022-02-25 20:38:48 +0300

    Favorite(Show favorite)

--- lib/features/home/presentation/widgets/product_grid_view.dart
+++ lib/features/home/presentation/widgets/product_grid_view.dart
@@ -1,109 +1,131 @@
 import 'package:flutter/material.dart';
-import 'package:salla/features/home/data/models/home_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:salla/features/home/data/models/product_model.dart';
+import 'package:salla/features/home/presentation/bloc/home_bloc.dart';
 
-Widget productGridView({required List<ProductModel> products,ScrollPhysics? physics}) {
-  return GridView.count(
-    shrinkWrap: true,
-    physics: physics??const NeverScrollableScrollPhysics(),
-    crossAxisCount: 2,
-    crossAxisSpacing: 5,
-    mainAxisSpacing: 5,
-    childAspectRatio: 1 / 1.5,
-    children: List.generate(
-      products.length,
-          (index) {
-        return Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: Colors.black12)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 4,
-                child: Stack(
-                  alignment: AlignmentDirectional.bottomStart,
+class ProductGridView extends StatelessWidget {
+  final List<ProductModel> products;
+  final ScrollPhysics? physics;
+
+  const ProductGridView({Key? key, required this.products, this.physics})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var homeBloc = HomeBloc.get(context);
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        return GridView.count(
+          shrinkWrap: true,
+          physics: physics ?? const NeverScrollableScrollPhysics(),
+          crossAxisCount: 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          childAspectRatio: 1 / 1.5,
+          children: List.generate(
+            products.length,
+            (index) {
+              return Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Colors.black12)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Image(
-                      image: NetworkImage(products[index].image),
-                      height: 250,
-                      width: double.infinity,
-                    ),
-                    if (products[index].discount > 0)
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
-                        child: const Text(
-                          'Discount',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        color: Colors.red,
+                    Expanded(
+                      flex: 4,
+                      child: Stack(
+                        alignment: AlignmentDirectional.bottomStart,
+                        children: [
+                          Image(
+                            image: NetworkImage(products[index].image),
+                            height: 250,
+                            width: double.infinity,
+                          ),
+                          if (products[index].discount > 0)
+                            Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5),
+                              child: const Text(
+                                'Discount',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              color: Colors.red,
+                            ),
+                        ],
                       ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                products[index].name,
+                                maxLines: 2,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                            Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.grey.withOpacity(0.1),
+                              ),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    products[index].price.round().toString(),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  if (products[index].discount > 0)
+                                    Text(
+                                      products[index]
+                                          .oldPrice
+                                          .round()
+                                          .toString(),
+                                      style: const TextStyle(
+                                        decoration: TextDecoration.lineThrough,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  const Spacer(),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon:
+                                        homeBloc.favorite[products[index].id] ??
+                                                false
+                                            ? const Icon(
+                                                Icons.favorite,
+                                                color: Colors.red,
+                                              )
+                                            : const Icon(
+                                                Icons.favorite_border,
+                                              ),
+                                    padding: EdgeInsets.zero,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                  Expanded(
-                  child: Text(
-                  products[index].name,
-                    maxLines: 2,
-                    overflow: TextOverflow.clip,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                    color: Colors.grey.withOpacity(0.1),
-
-                  ),
-                child: Row(
-                  children: [
-                    Text(
-                      products[index].price
-                          .round()
-                          .toString(),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    if (products[index].discount > 0)
-                      Text(
-                        products[index].oldPrice
-                            .round()
-                            .toString(),
-                        style: const TextStyle(
-                          decoration: TextDecoration.lineThrough,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    const Spacer(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.favorite_border),
-                      padding: EdgeInsets.zero,
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              );
+            },
           ),
-        ),)
-        ,
-        ]
-        ,
-        )
-        ,
         );
       },
-    ),
-  );
+    );
+  }
 }
