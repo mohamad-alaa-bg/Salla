a8636f4940287cdcc4b6c5bccb7696c41e8ee5c4
Mohamad Alaa Baghdadi <newmhd.alaa.bg@gmail.com>
2022-02-25 23:57:23 +0300

    Favorite(favorite screen bloc)

---
+++ lib/features/favorites/presentation/bloc/favorites_bloc.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'favorites_event.dart';
+part 'favorites_state.dart';
+
+class FavoritesBloc extends Bloc<FavoritesEvent, FavoritesState> {
+  FavoritesBloc() : super(FavoritesInitial()) {
+    on<FavoritesEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+
+  FavoritesBloc get(BuildContext context) =>BlocProvider.of(context);
+}
