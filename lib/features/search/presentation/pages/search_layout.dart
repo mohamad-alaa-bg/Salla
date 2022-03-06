7929caa7a71d137b76b0f6f3d962d2cec62051df
Mohamad Alaa Baghdadi <newmhd.alaa.bg@gmail.com>
2022-03-07 00:02:14 +0300

    Search Screen

--- lib/features/search/presentation/pages/search_layout.dart
+++ lib/features/search/presentation/pages/search_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:salla/core/widgets/custom_text_form_field.dart';
 import 'package:salla/features/home/presentation/widgets/product_grid_view.dart';
 import 'package:salla/features/search/presentation/bloc/search_bloc.dart';
 
@@ -12,11 +13,12 @@ class SearchLayout extends StatefulWidget {
 
 class _SearchLayoutState extends State<SearchLayout> {
   var searchBloc;
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     searchBloc = SearchBloc.get(context);
-    searchBloc.add(SearchProductsEvent(text: 'iphone'));
+     searchBloc.add(SearchProductsEvent(text: ''));
     super.initState();
   }
 
@@ -26,12 +28,37 @@ class _SearchLayoutState extends State<SearchLayout> {
       appBar: AppBar(),
       body: BlocBuilder<SearchBloc, SearchState>(
         builder: (context, state) {
-          return state is SearchLoadedSuccessState
-              ? ProductGridView(
-                  products: state.searchProduct.products,
-                  physics: const BouncingScrollPhysics(),
-                )
-              : Container();
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                CustomTextFormField(
+                  textEditingController: searchController,
+                  label: 'Search',
+                  onSubmitted: (value) => searchBloc
+                      .add(SearchProductsEvent(text: searchController.text)),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      searchBloc.add(
+                          SearchProductsEvent(text: searchController.text));
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                state is SearchLoadedSuccessState
+                    ? Expanded(
+                        child: ProductGridView(
+                          products: state.searchProduct.products,
+                          physics: const BouncingScrollPhysics(),
+                        ),
+                      )
+                    : const LinearProgressIndicator()
+              ],
+            ),
+          );
         },
       ),
     );
